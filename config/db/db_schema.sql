2502abf56e3f76ce8dd6f19cc120365fd3cc1742
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2022-11-14 00:57:31 +0100

    Fix deployement in local

--- config/db/db_schema.sql
+++ config/db/db_schema.sql
@@ -1,37 +1,26 @@
 CREATE DATABASE IF NOT EXISTS `csa1`;
+USE csa1 ;
 
-CREATE USER 'dbusercsa1' IDENTIFIED BY 'thisDefaultUserPasswordMustBeChanedInProduction';
+DROP USER IF EXISTS 'dbusercsa1'@'172.16.0.4';
+DROP USER IF EXISTS 'dbusercsa1_ReadOnlyUser'@'%';
+DROP USER IF EXISTS 'dbusercsa1_AdminUser'@'%';
 
-GRANT USAGE ON `csa1`.* TO 'dbusercsa1'@'%' IDENTIFIED BY 'thisDefaultUserPasswordMustBeChanedInProduction';
-GRANT ALL privileges ON `csa1`.* TO 'dbusercsa1'@'%';
+CREATE USER 'dbusercsa1'@'172.16.0.4' IDENTIFIED BY 'thisDefaultUserPasswordMustBeChanedInProduction' REQUIRE NONE;
+CREATE USER 'dbusercsa1_ReadOnlyUser'@'%' IDENTIFIED BY 'thisDefaultUserPasswordMustBeChanedInProduction' REQUIRE SSL;
+CREATE USER 'dbusercsa1_AdminUser'@'%' IDENTIFIED BY 'thisDefaultUserPasswordMustBeChanedInProduction' REQUIRE SSL;
+
+GRANT USAGE ON `csa1`.* TO 'dbusercsa1'@'172.16.0.4';
+GRANT ALL privileges ON `csa1`.* TO 'dbusercsa1'@'172.16.0.4';
+GRANT USAGE ON `csa1`.* TO 'dbusercsa1_AdminUser'@'%';
+GRANT ALL privileges ON `csa1`.* TO 'dbusercsa1_AdminUser'@'%';
+GRANT SELECT ON `csa1`.* TO 'dbusercsa1_ReadOnlyUser'@'%';
 FLUSH PRIVILEGES;
 
-CREATE TABLE IF NOT EXISTS csa1.persistence_metadata (
-  persistence_key BIGINT NOT NULL AUTO_INCREMENT,
-  persistence_id VARCHAR(255) NOT NULL,
-  sequence_nr BIGINT NOT NULL,
-  PRIMARY KEY (persistence_key),
-  UNIQUE (persistence_id)
-) CHARACTER SET utf8 COLLATE = 'utf8_unicode_ci';
+ALTER TABLE IF EXISTS persistence_journal RENAME archive_persistence_journal;
+ALTER TABLE IF EXISTS persistence_metadata RENAME archive_persistence_metadata;
+ALTER TABLE IF EXISTS persistence_snapshot RENAME archive_persistence_snapshot;
 
-CREATE TABLE IF NOT EXISTS csa1.persistence_journal (
-  persistence_key BIGINT NOT NULL,
-  sequence_nr BIGINT NOT NULL,
-  message MEDIUMBLOB NOT NULL,
-  PRIMARY KEY (persistence_key, sequence_nr),
-  FOREIGN KEY (persistence_key) REFERENCES persistence_metadata (persistence_key)
-) CHARACTER SET utf8 COLLATE = 'utf8_unicode_ci';
-
-CREATE TABLE IF NOT EXISTS csa1.persistence_snapshot (
-  persistence_key BIGINT NOT NULL,
-  sequence_nr BIGINT NOT NULL,
-  created_at BIGINT NOT NULL,
-  snapshot MEDIUMBLOB NOT NULL,
-  PRIMARY KEY (persistence_key, sequence_nr),
-  FOREIGN KEY (persistence_key) REFERENCES persistence_metadata (persistence_key)
-) CHARACTER SET utf8 COLLATE = 'utf8_unicode_ci';
-
-CREATE TABLE IF NOT EXISTS csa1.DBSchema (
+CREATE TABLE IF NOT EXISTS DBSchema (
   id BIGINT NOT NULL,
   revision BIGINT NOT NULL,
   status varchar(50) NOT NULL,
@@ -40,9 +29,9 @@ CREATE TABLE IF NOT EXISTS csa1.DBSchema (
   modifidat DATETIME NOT NULL,
   modifikator BIGINT NOT NULL,
   PRIMARY KEY (id)
-) CHARACTER SET utf8 COLLATE = 'utf8_unicode_ci';
+) CHARACTER SET utf8mb4 COLLATE = 'utf8mb4_unicode_ci';
 
-CREATE TABLE IF NOT EXISTS csa1.PersistenceEventState  (
+CREATE TABLE IF NOT EXISTS PersistenceEventState  (
   id BIGINT not null,
   persistence_id varchar(100) not null,
   last_transaction_nr BIGINT default 0,
@@ -51,4 +40,46 @@ CREATE TABLE IF NOT EXISTS csa1.PersistenceEventState  (
   ersteller BIGINT not null,
   modifidat datetime not null,
   modifikator BIGINT not null
-) CHARACTER SET utf8 COLLATE = 'utf8_unicode_ci';
+) CHARACTER SET utf8mb4 COLLATE = 'utf8mb4_unicode_ci';
+
+CREATE TABLE IF NOT EXISTS event_journal(
+    ordering SERIAL,
+    deleted BOOLEAN DEFAULT false NOT NULL,
+    persistence_id VARCHAR(255) NOT NULL,
+    sequence_number BIGINT NOT NULL,
+    writer TEXT NOT NULL,
+    write_timestamp BIGINT NOT NULL,
+    adapter_manifest TEXT NOT NULL,
+    event_payload MEDIUMBLOB NOT NULL,
+    event_ser_id INTEGER NOT NULL,
+    event_ser_manifest TEXT NOT NULL,
+    meta_payload MEDIUMBLOB,
+    meta_ser_id INTEGER,meta_ser_manifest TEXT,
+    PRIMARY KEY(persistence_id,sequence_number)
+) CHARACTER SET utf8mb4 COLLATE = 'utf8mb4_unicode_ci';
+
+CREATE UNIQUE INDEX IF NOT EXISTS event_journal_ordering_idx ON event_journal(ordering);
+
+CREATE TABLE IF NOT EXISTS event_tag (
+    event_id BIGINT UNSIGNED NOT NULL,
+    tag VARCHAR(255) NOT NULL,
+    PRIMARY KEY(event_id, tag),
+    FOREIGN KEY (event_id)
+        REFERENCES event_journal(ordering)
+        ON DELETE CASCADE
+    ) CHARACTER SET utf8mb4 COLLATE = 'utf8mb4_unicode_ci';
+
+CREATE TABLE IF NOT EXISTS snapshot (
+    persistence_id VARCHAR(255) NOT NULL,
+    sequence_number BIGINT NOT NULL,
+    created BIGINT NOT NULL,
+    snapshot_ser_id INTEGER NOT NULL,
+    snapshot_ser_manifest TEXT NOT NULL,
+    snapshot_payload MEDIUMBLOB NOT NULL,
+    meta_ser_id INTEGER,
+    meta_ser_manifest TEXT,
+    meta_payload MEDIUMBLOB,
+  PRIMARY KEY (persistence_id, sequence_number)) CHARACTER SET utf8mb4 COLLATE = 'utf8mb4_unicode_ci';
+
+CREATE OR REPLACE VIEW csa1.journal_view AS
+  SELECT 2 AS journal_version, persistence_id, sequence_number as sequence_nr, event_payload as message FROM event_journal;
